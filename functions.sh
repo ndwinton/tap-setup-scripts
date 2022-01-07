368ef4bac1357be861c2a3e365b7958b8402072a
Neil Winton <nwinton@pivotal.io>
2022-01-07 13:57:15 +0000

    ClusterImagePolicy API version change

--- functions.sh
+++ functions.sh
@@ -757,7 +757,7 @@ EOF
     banner "Creating basic ClusterImagePolicy"
 
     cat <<EOF | kubectl apply -f -
-apiVersion: signing.run.tanzu.vmware.com/v1alpha1
+apiVersion: signing.run.tanzu.vmware.com/v1beta1
 kind: ClusterImagePolicy
 metadata:
  name: image-policy
